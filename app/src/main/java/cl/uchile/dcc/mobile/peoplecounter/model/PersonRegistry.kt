aad404e243ab90db8f2545f6e10fe1467027736d
frantapia <106940872+frantapiarozas@users.noreply.github.com>
2026-08-13 16:45:33 -0400

    Delete app directory

--- app/src/main/java/cl/uchile/dcc/mobile/peoplecounter/model/PersonRegistry.kt
+++
@@ -1,9 +0,0 @@
-package cl.uchile.dcc.mobile.peoplecounter.model
-
-data class PersonRegistry(
-    var nombre: String,
-    var edad: Int,
-    var genero: String,
-    var fecha: String,
-    var hora: String
-)
